4da78943ee1e270bda60cafe25977e4f328af5fc
Charles Xu <charl3s.xu@gmail.com>
2017-09-23 20:49:15 +0800

    updated structure, readme, and installation script

---
+++ README.md
@@ -0,0 +1,15 @@
+# CharlesTermux
+
+This project is a subproject of [CharlesScripts](https://github.com/the0demiurge/CharlesScripts), you can star/fork/pr this or that project :)
+
+## Installation
+
+run `apt update;apt install -y curl;bash -c "$(curl -fsSL https://git.io/vdUkk)"`
+
+## Features
+
+- **Beautiful powerline** fonts and powerline theme for fish, tmux and ipython
+- Solarized Dark Theme
+- Auto add two sources, which makes **you have gcc-7, octave... in source**
+- My subset of [CharlesScripts](https://github.com/the0demiurge/CharlesScripts), and you'll have **`inst`, `ef`, `fifo`, `aria2c-daemon-start`, and `get-path`...**
+- A bounch of convenient shell aliases, such as `g` command for git.You'll see them in [shell.rc.d](https://github.com/the0demiurge/CharlesTermux/tree/master/charles/shell.rc.d)
---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+apt update
+apt install -y git fish curl apt-transport-https gawk tmux vim-python python cowsay
+pip install ipython powerline-status
+clear
+
+if [ -d "$HOME/.termux" ]; then
+ mv $HOME/.termux $HOME/.termux.bak
+fi
+
+# Setting sources
+echo '# The termux repository mirror from TUNA:'|tee $HOME/../usr/etc/apt/source.list.d/tuna.list
+echo 'deb [arch=all,arm] http://mirrors.tuna.tsinghua.edu.cn/termux stable main'|tee -a $HOME/../usr/etc/apt/source.list.d/tuna.list
+
+git clone https://github.com/the0demiurge/CharlesTermux.git $HOME/.local/share/CharlesTermux
+
+cp -r ~/.local/share/CharlesTermux/data/.termux ~
+
+cd ~/.local/share/CharlesTermux/charles/installation.d/conf.d
+
+./config-bash
+./config-fish
+./config-git
+./config-powerline-ipython
+./config-tmux
+
+echo 'After installation, you can edit $CHARLES_BACKUP/termux.list and run `inst (inst) in fish shell to install listed packages automitacally.`'
+echo 'After installed oh-my-fish, you can run `omf-backup` to backup omf list recorded in $CHARLES_BACKUP/omf.list and run `omf-restore` to restore.'
+read
+
+termux-setup-storage
+curl -L https://get.oh-my.fish | fish
+
+echo Done!
+
+exit
